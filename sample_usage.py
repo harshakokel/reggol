97f9f0d0dc77a0a940674613f2a20de7926c0fa7
Harsha <harshakokel@gmail.com>
2021-10-22 12:47:10 -0500

    updating sample usage

--- sample_usage.py
+++ sample_usage.py
@@ -2,13 +2,19 @@ from reggol import setup_logger
 import os
 import time
 
-variant = {'exp_prefix': "custom_experminet",
+variant = {'exp_prefix': "custom_experiment",
            'algorithm': "baseline",
            'domain': "gridworld",
            'batch_size': 5,
            'learning_rate': 0.02}
 
-logger = setup_logger(variant['exp_prefix'], variant, exp_id=os.getpid())
+logger = setup_logger(variant['exp_prefix'],
+                      variant,
+                      exp_id=os.getpid(),
+                      tabular_log_file='record.csv',
+                      text_log_file='output.log',
+                      variant_log_file='dict.json',
+                      base_log_dir='./sample_usage_log/')
 
 logger.log("Starting Experiment")
 for i in range(5):
