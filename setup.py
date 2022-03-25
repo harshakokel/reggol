2c1c9f18474dccaa9a7e7d7e1784f06e527dda85
Harsha <harshakokel@gmail.com>
2022-03-25 01:11:43 -0500

    Adding requirement

--- setup.py
+++ setup.py
@@ -8,5 +8,9 @@ setup(
     license='',
     author='Harsha Kokel',
     author_email='harshakokel@gmail.com',
-    description='Logger util'
+    description='Logger util',
+    install_requires=["numpy >= 1.14.2",
+                      "GitPython~=3.1.24",
+                      "python-dateutil~=2.7.5"
+                     ],
 )
